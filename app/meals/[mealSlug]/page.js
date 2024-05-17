8b14826f1970d31534aad9865ac8e5e13766438a
Arjit Prajapati <arjitprajapati@gmail.com>
2024-05-17 20:37:00 +0530

    Compiled

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,6 +2,26 @@ import classes from './page.module.css'
 import Image from 'next/image'
 import { getMeal } from '@/lib/meals'
 import { notFound } from 'next/navigation'
+
+export async function generateMetadata({params}){ //for adding metadata to a dynamic page
+    const meal =getMeal(params.mealSlug);
+
+    if(!meal){
+        notFound()
+         } 
+         
+    return {
+        title :meal.title,
+        description:meal.summary,
+    }
+    
+}
+
+
+
+
+
+
 export default function MealDetailPage({params}){
 
  const meal =getMeal(params.mealSlug)
--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import MealsGrid from '@/component/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 import { Suspense } from 'react';
 
+export const metadata={
+    title:'All meals',
+    description:'Browse the delicious meals shared by our vibrant community.'
+}
+
 async function Meals(){
     const meals =await getMeals()
     return <MealsGrid meals={meals}/>
